a5c7c36614418da26c86d0986aa2672db0968ce2
Jeslyn Tatang <jeslyn.tatang@gmail.com>
2018-10-10 01:09:53 +0000

    .val and styling

--- js/script.js
+++ js/script.js
@@ -2,10 +2,37 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 
-//GLOBAL VARIABLES
 
+//GLOBAL VARIABLES
 
 
 
 // DOCUMENT READY FUNCTION BELOW
 
+
+/* 
+
+[ ] Outside of your click handler, declare a randomNumber variable
+and set it equal to 0
+
+[ ] In your click handler, generate a random number and assign it to
+the randomNumber variable
+
+[ ] In your click handler, store the input value in the userChoice
+variable
+
+[ ] In your click handler, display userChoice to the screen */
+
+
+$(document).ready(function() {
+    
+    $("#shoot").click(function() {
+        $("#userChoice").text($("#input").val());
+    });
+    
+    var userChoice = "";
+    var computerChoice = "";
+    var winner = "";
+    var randomNumber = 0;
+    
+});
