184cebf076873dbdc02bf6139e30b104b0fc1771
Jeslyn Tatang <jeslyn.tatang@gmail.com>
2018-10-17 01:01:18 +0000

    computer uses random numbers to play

--- js/script.js
+++ js/script.js
@@ -26,13 +26,68 @@ variable
 
 $(document).ready(function() {
     
-    $("#shoot").click(function() {
-        $("#userChoice").text($("#input").val());
-    });
-    
     var userChoice = "";
     var computerChoice = "";
     var winner = "";
     var randomNumber = 0;
     
+    $("#shoot").click(function() {
+        var userInput = $("#input").val();
+        randomNumber = Math.floor((Math.random() * 3) + 1); // range: btwn 1-3 inclusive
+        if ((userInput === "rock") || (userInput === "paper") || (userInput === "scissors")) {
+            userChoice === userInput;
+            $("#userChoice").text(userInput);
+            $("#result").text("");
+        }
+        else {
+            $("#userChoice").text("");
+            $("#computerChoice").text(" ");
+            $("#result").text("Please enter 'rock,' 'paper,' or 'scissors'");
+        }
+        if (randomNumber === 1) {
+            computerChoice = "rock";
+            $("#computerChoice").text(computerChoice);
+        }
+        else if (randomNumber === 2) {
+            computerChoice = "paper";
+            $("#computerChoice").text(computerChoice);
+        }
+        else if (randomNumber === 3) {
+            computerChoice = "scissors";
+            $("#computerChoice").text(computerChoice);
+        }
+        if (computerChoice === "paper") {
+            if (userInput === "paper") {
+                $("#result").text("Tie - No one wins");
+            }
+            else if (userInput === "scissors") {
+                $("#result").text("Player wins");
+            }
+            else if (userInput === "rock") {
+                $("#result").text("Computer wins");
+            }
+        }
+        if (computerChoice === "rock") {
+            if (userInput === "rock") {
+                $("#result").text("Tie - No one wins");
+            }
+            else if (userInput === "paper") {
+                $("#result").text("Player wins");
+            }
+            else if (userInput === "scissors") {
+                $("#result").text("Computer wins");
+            }
+        }
+        if (computerChoice === "scissors") {
+            if (userInput === "scissors") {
+                $("#result").text("Tie - No one wins");
+            }
+            else if (userInput === "rock") {
+                $("#result").text("Player wins");
+            }
+            else if (userInput === "paper") {
+                $("#result").text("Computer wins");
+            }
+        }
+    });
 });
